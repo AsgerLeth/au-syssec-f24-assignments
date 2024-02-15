03109fbba11f06488d8f3466b60e9a7ceb8c9396
AsgerLeth <202004818@post.au.dk>
2024-02-15 14:19:48 +0100

    Med korrekt padding

--- crypto/cbc-padding-oracle/example-solve.py
+++ crypto/cbc-padding-oracle/example-solve.py
@@ -5,14 +5,68 @@
 
 import requests
 import sys
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Cipher import AES
+
+def single_block(base_url):
+    BLOCK_SIZE = 16
+    zeroing_iv = [0] * BLOCK_SIZE
+    cookie = requests.get(f'{base_url}')
+    cookie_header = cookie.headers.get('Set-Cookie')    
+    authtoken = cookie_header.split('=')[1].split(';')[0]
+    new_ciphertext = bytes.fromhex(authtoken)
+    res = requests.get(f'{base_url}/quote/', cookies={'authtoken': new_ciphertext.hex()})
+
+    for pad_val in range(1, BLOCK_SIZE+1):
+        padding_iv = [pad_val ^ b for b in zeroing_iv]
+
+        for candidate in range(256):
+            padding_iv[-pad_val] = candidate
+            iv = bytes(padding_iv)
+            if res != "Padding is incorrect.":
+                if pad_val == 1:
+                    # make sure the padding really is of length 1 by changing
+                    # the penultimate block and querying the oracle again
+                    padding_iv[-2] ^= 1
+                    iv = bytes(padding_iv)
+                    if not res != "Padding is incorrect.":
+                        continue  # false positive; keep searching
+                break
+        else:
+            raise Exception("no valid padding byte found (is the oracle working correctly?)")
+
+        zeroing_iv[-pad_val] = candidate ^ pad_val
+
+    return zeroing_iv
+
+def full_attack(iv,ct):
+    msg = iv
+    blocks = [msg[i:i+16] for i in range(0, len(msg), 16)]
+    result = b''
+    iv = blocks[0]
+    for ct in blocks[1:]:
+        dec = single_block(ct)
+        pt = bytes(iv_byte ^ dec_byte for iv_byte, dec_byte in zip(iv, dec))
+        result += pt
+        iv = ct
+    return result
 
 def test_systems_security(base_url):
-    new_ciphertext = bytes.fromhex('2cc9a9fc7cb4dc60f1df7babc4bf82c1122b12cbd8a1c10e1d7f1d4cf57c60ed8cb3703e30ff4b1a2a9af418df999c71b331721a24e713668d0478351a4ccad77fa6abff498d919b3773e6e25fcad5556545a6339b9d4f42c854f96e940a538342424242424242424242424242424242')
+    cookie = requests.get(f'{base_url}')
+    cookie_header = cookie.headers.get('Set-Cookie')    
+    authtoken = cookie_header.split('=')[1].split(';')[0]
+    new_ciphertext = bytes.fromhex(authtoken)
     res = requests.get(f'{base_url}/quote/', cookies={'authtoken': new_ciphertext.hex()})
+    zero_iv = single_block(base_url)
+    result = full_attack(zero_iv,base_url)
+    #print(zero_iv)
+    print(result)
     print(f'[+] done:\n{res.text}')
+    print("cookie:", authtoken)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print(f'usage: {sys.argv[0]} <base url>', file=sys.stderr)
         exit(1)
     test_systems_security(sys.argv[1])
+
