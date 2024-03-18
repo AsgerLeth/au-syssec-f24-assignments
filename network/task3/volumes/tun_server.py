d6a1978723e675e4b55951570a145fe7fdcae2f7
sigurd <sigurd@Ubuntu.myguest.virtualbox.org>
2024-03-18 16:11:34 +0100

    changed to stream

--- network/task3/volumes/tun_server.py
+++ network/task3/volumes/tun_server.py
@@ -11,7 +11,7 @@ IFF_NO_PI = 0x1000
 
 IP_A = "0.0.0.0" # Listen on all interfaces
 PORT = 9090
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((IP_A, PORT))
 
 # Create a TUN interface
@@ -23,19 +23,20 @@ ifname_bytes = fcntl.ioctl(tun, TUNSETIFF, ifr)
 os.system("ifconfig tun0 10.8.0.1 netmask 255.255.255.0 up")
 
 # Create UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 
 # Load the server's certificate and private key
-context.load_cert_chain(certfile="./volumes/cert.pem")
+context.load_cert_chain(certfile="cert.pem")
 
 # Wrap the server's socket with the SSL context
-secure_sock = context.wrap_socket(sock, server_side=True)
+
 
 while True:
     # Accept a new connection
-    client_sock, client_address = secure_sock.accept()
+    client_sock, client_address = sock.accept()
+    secure_sock = context.wrap_socket(sock, server_side=True)
     while True:
         data, (ip, port) = sock.recvfrom(2048)
         print("{}:{} --> {}:{}".format(ip, port, IP_A, PORT))
